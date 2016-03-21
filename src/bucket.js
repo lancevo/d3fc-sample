7fbd0c888656510b57683da35bc78e877f067d92
William Ferguson <wferguson@scottlogic.com>
2016-03-21 08:30:35 +0000

    refactor: Bucketer to use () => syntax

--- src/bucket.js
+++ src/bucket.js
@@ -4,13 +4,8 @@ export default function() {
 
     var bucketSize = 10;
 
-    var bucket = function(data) {
-        var numberOfBuckets = Math.ceil(data.length / bucketSize);
-
-        return d3.range(0, numberOfBuckets).map((i) => {
-            return data.slice(i * bucketSize, (i + 1) * bucketSize);
-        });
-    };
+    var bucket = (data) => d3.range(0, Math.ceil(data.length / bucketSize))
+                            .map((i) => data.slice(i * bucketSize, (i + 1) * bucketSize));
 
     bucket.bucketSize = function(x) {
         if (!arguments.length) {
